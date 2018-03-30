7d94b7ebebec5c5945b3cd14d1631cf3dd5d3bf3
Dan <elite.wisdom@gmx.com>
2018-03-30 04:38:50 -0400

    hull id in admin

--- intrepidboats/apps/common/admin.py
+++ intrepidboats/apps/common/admin.py
@@ -53,7 +53,7 @@ class EventAdmin(ModelAdmin):
 class ExtraUserDataInline(admin.TabularInline):
     can_delete = False
     model = ExtraUserData
-    fields = ['profile_picture', 'gallery_header']
+    fields = ['profile_picture', 'gallery_header', 'intrepid_hull_id']
 
     class Media:
         css = {"all": ("css/admin/hide_inline_object_name.css",)}
@@ -66,8 +66,8 @@ class UserAdmin(BaseUserAdmin):
 @admin.register(ExtraUserData)
 class DetailsForStaffAdmin(ModelAdmin):
     model = ExtraUserData
-    fields = ['user', 'purchased_boat_info', 'details_for_staff']
-    list_display = ['user', 'truncated_purchased_boat_info', 'truncated_details_for_staff']
+    fields = ['user', 'purchased_boat_info', 'details_for_staff', 'intrepid_hull_id']
+    list_display = ['user', 'truncated_purchased_boat_info', 'truncated_details_for_staff', 'intrepid_hull_id']
     search_fields = ['user__username']
 
     def get_readonly_fields(self, request, obj=None):
